348110adb0368c02d0cd36bded6d5d8dc1e843b3
Shin <shinnguyen93@gmail.com>
2021-09-03 19:38:35 +0930

    New local_settings file

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -124,3 +124,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
